cac5e0434919b0111f66509f61a051d3c0be487d
HypersunTT <speedhans87@gmail.com>
2020-02-03 23:31:16 +0900

    add BossUI(alpha)

---
+++ Assets/Project/Scripts/UISystem/BossCharacterUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossCharacterUI : DefaultUI
+{
+    class BossUIData
+    {
+        List<SpriteRenderer> m_ListBuffIcon = new List<SpriteRenderer>();
+        Character m_Character;
+
+        public void SetCharacter(Character _Target)
+        {
+            m_Character = _Target;
+        }
+    }
+
+
+    [SerializeField]
+    float m_UpdateDelay = 0.5f;
+    float m_UpdateTimer;
+
+}
